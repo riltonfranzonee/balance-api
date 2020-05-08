8f0991e7a64e7d838f0bebcf53f1ac1dc4acd094
Rilton Franzone <riltonfranzone@gmail.com>
2020-05-08 18:51:04 -0300

    [feat]: create, list transactions and calculate balance

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,62 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const { income, outcome } = this.transactions.reduce(
+      (sum: Balance, currentTransaction: Transaction) => {
+        switch (currentTransaction.type) {
+          case 'income':
+            sum.income += currentTransaction.value;
+            break;
+          case 'outcome':
+            sum.outcome += currentTransaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return sum;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
